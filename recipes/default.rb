a40d42fd4d90a48bfb492910b7ac11d34f64e6f3
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-12-03 12:45:39 -0500

    Attempt to make the recipe work.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# cookbook-syslog-ng/recipes/default.rb
+
+include_recipe "apache2"
+
+# Install the generic vhosts file.
+template "httpd-vhosts" do
+  path "#{node['apache']['dir']}/conf.d/httpd-vhosts.conf"
+  source "httpd-vhosts.conf.erb"
+  owner "root"
+  group node['apache']['root_group']
+  mode 00644
+  backup false
+  notifies :restart, "service[apache2]"
+end
