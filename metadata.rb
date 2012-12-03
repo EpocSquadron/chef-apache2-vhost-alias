e16fe824cff0e0b28af64c504ba2cb74ead98549
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-12-03 17:46:09 -0500

    Adjust metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Digital Surgeons"
+maintainer_email "dan.poulin@digitalsurgeons.com"
 license          "All rights reserved"
 description      "Installs/Configures dynamic-vhosts"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
