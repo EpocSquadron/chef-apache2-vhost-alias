5e29039dfb984bc76d39f5d023b84eb1e2c7b084
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-12-05 16:22:58 -0500

    Go ahead and just use this recipe to call the others, make mysql accesible.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,8 +9,35 @@
 
 # cookbook-syslog-ng/recipes/default.rb
 
+# Set up the entire LAMP stack
+include_recipe "apt"
+include_recipe "openssl"
+include_recipe "mysql::server"
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "php::module_curl"
 include_recipe "apache2"
+include_recipe "apache2::mod_ssl"
+include_recipe "apache2::mod_rewrite"
+include_recipe "apache2::mod_headers"
+include_recipe "apache2::mod_deflate"
+include_recipe "apache2::mod_php5"
 include_recipe "dynamic-vhosts::mod_vhost_alias"
+
+# Allow host system to connect to mysql
+# From: https://github.com/afhole/vagrant-lamp/blob/master/cookbooks/vagrant_main/recipes/default.rb
+mysql_connection_info = {
+	:host => "localhost",
+	:username => 'root',
+	:password => node['mysql']['server_root_password']
+}
+
+mysql_database_user 'root' do
+  connection mysql_connection_info
+  password node['mysql']['server_root_password']
+  host "192.168.0.1"
+  action :grant
+end
 
 # Install the generic vhosts file.
 template "httpd-vhosts.conf" do
