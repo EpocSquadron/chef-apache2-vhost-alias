a7d9ac31f21bccc2992ccb809ffe19de46fb6c6e
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-12-05 11:21:56 -0500

    Add in docroot correction stuff.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,14 +11,36 @@
 
 include_recipe "apache2"
 include_recipe "apache2::mod_vhost_alias"
+include_recipe "php"
 
 # Install the generic vhosts file.
 template "httpd-vhosts.conf" do
-  path "#{node['apache']['dir']}/conf.d/httpd-vhosts.conf"
-  source "httpd-vhosts.conf.erb"
-  owner "root"
-  group node['apache']['root_group']
-  mode 00644
-  backup false
-  notifies :restart, "service[apache2]"
+	path "#{node['apache']['dir']}/conf.d/httpd-vhosts.conf"
+	source "httpd-vhosts.conf.erb"
+	owner "root"
+	group node['apache']['root_group']
+	mode 00644
+	backup false
+	notifies :restart, "service[apache2]"
+end
+
+if node['dynamic-vhosts']['using_php']
+
+	# Make a directory for includes.
+	directory "#{node['apache']['dir']}/includes" do
+	  mode 00755
+	  owner "root"
+	  group node['apache']['root_group']
+	end
+
+	# Insert the auto_prepend_file
+	template "setDocRoot.php" do
+		path "#{node['apache']['dir']}/includes/setDocRoot.php"
+		source "setDocRoot.php.erb"
+		owner "root"
+		group node['apache']['root_group']
+		mode 00644
+		backup false
+		notifies :restart, "service[apache2]"
+	end
 end
