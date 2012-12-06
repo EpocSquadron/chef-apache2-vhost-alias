03755eaca5e76088d930f0f1f493b6d7f1a35501
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-12-05 19:14:02 -0500

    Truns out we don't need to set the user to do stuff.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,24 +8,7 @@
 # cookbook-syslog-ng/recipes/default.rb
 
 include_recipe "apache2"
-include_recipe "mysql::server"
-include_recipe "database"
 include_recipe "dynamic-vhosts::mod_vhost_alias"
-
-# Allow host system to connect to mysql
-# From: https://github.com/afhole/vagrant-lamp/blob/master/cookbooks/vagrant_main/recipes/default.rb
-mysql_connection_info = {
-	:host => "localhost",
-	:username => 'root',
-	:password => node['mysql']['server_root_password']
-}
-
-mysql_database_user 'root' do
-  connection mysql_connection_info
-  password node['mysql']['server_root_password']
-  host "192.168.33.1"
-  action :grant
-end
 
 # Install the generic vhosts file.
 template "httpd-vhosts.conf" do
